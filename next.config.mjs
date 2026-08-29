a02c20815434d00946f82b0825bd0227e15e50b0
Aryan Jadhav <aretos248@gmail.com>
2026-08-29 18:49:50 +0530

    proxy

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 const nextConfig = {
   reactStrictMode: true,
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://54.243.210.131:8000/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
