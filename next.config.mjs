5e5c92d52b52ed3dcd9f5df4687cd1fbd480ce65
Aryan Jadhav <aretos248@gmail.com>
2026-08-30 08:36:10 +0530

    routing

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,5 @@
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: true,
-
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*/",
-        destination: "http://54.243.210.131:8000/api/:path*/",
-      },
-    ];
-  },
 };
 
 export default nextConfig;
